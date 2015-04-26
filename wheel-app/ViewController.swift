8beec4964f4fe788de3c3f5013265015b0dd7ce2
Mobile Innovation, LLC <amatani@mobile-innovation.jp>
2015-04-26 13:16:12 +0900

    Initial Commit

---
+++ wheel-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  wheel-app
+//
+//  Created by MacServer on 2015/04/26.
+//  Copyright (c) 2015年 Mobile Innovation, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
